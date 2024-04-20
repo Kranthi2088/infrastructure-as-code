f55931a4ff475d595acb4164b766db4abea4537d
Dunna, Kranthi Kumar <kranthiwork08@gmail.com>
2024-04-20 16:25:35 -0400

    terrformv1

--- resource-group.tf
+++ resource-group.tf
@@ -3,6 +3,6 @@
 # Creates a resource group in our Azure account.
 #
 resource "azurerm_resource_group" "practicum16" {
-  name     = "practicum16"
-  location = "eastus"
+  name     = var.app_name
+  location = var.location
 }
